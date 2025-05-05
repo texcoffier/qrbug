4454bb3bc172c943a2812d21a2d8a0aaa9ef3db0
Mathieu <nope@example.com>
2025-05-05 16:41:03 +0200

    Reformat to use 4 spaces instead of tabs

--- src/qrbug/user.py
+++ src/qrbug/user.py
@@ -7,25 +7,25 @@ UserId: TypeAlias = str
 
 
 class User:
-	instances: dict[UserId, "User"] = {}
+    instances: dict[UserId, "User"] = {}
 
-	def __init__(self, user: UserId):
-		self.user_id = user
-		self.children_ids = []
+    def __init__(self, user: UserId):
+        self.user_id = user
+        self.children_ids = []
 
-	def user_add(self, child: "User") -> None:
-		assert child.user_id not in self.children_ids
-		self.children_ids.append(child.user_id)
+    def user_add(self, child: "User") -> None:
+        assert child.user_id not in self.children_ids
+        self.children_ids.append(child.user_id)
 
-	@classmethod
-	def get(cls, user: UserId) -> "User":
-		if user not in User.instances:
-			User.instances[user] = User(user)
-		return User.instances[user]
+    @classmethod
+    def get(cls, user: UserId) -> "User":
+        if user not in User.instances:
+            User.instances[user] = User(user)
+        return User.instances[user]
 
-	def dump(self) -> str:
-		return f"{self.user_id} {self.children_ids}"
+    def dump(self) -> str:
+        return f"{self.user_id} {self.children_ids}"
 
 
 def user_add(parent: UserId, child: UserId) -> None:
-	User.get(parent).user_add(User.get(child))
+    User.get(parent).user_add(User.get(child))
