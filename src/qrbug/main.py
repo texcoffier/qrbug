1e8e4cc3daad0f362f94ee225f610484f990eaaa
Mathieu <no@example.com>
2025-05-07 15:48:18 +0200

    Added the header of the functions used in the failures journal

--- src/qrbug/main.py
+++ src/qrbug/main.py
@@ -1,12 +1,34 @@
-from qrbug.user import user_add, user_remove
-from qrbug.failure import failure_update, failure_add, failure_remove, DisplayTypes
-from qrbug.thing import thing_update, thing_del
-from qrbug.action import action
+import os
+from typing import Optional
+
+from qrbug.user import user_add, user_remove, UserId
+from qrbug.failure import failure_update, failure_add, failure_remove, DisplayTypes, FailureId
+from qrbug.thing import thing_update, thing_del, ThingId
+from qrbug.action import action, ActionId
 from qrbug.selector import selector
-from qrbug.dispatcher import dispatcher_update, dispatcher_del
+from qrbug.dispatcher import dispatcher_update, dispatcher_del, DispatcherId
 
 
-DB_FILE_PATH = "JOURNALS/db.py"
+JOURNALS_FILE_PATH = "JOURNALS/"
+DB_FILE_PATH = os.path.join(JOURNALS_FILE_PATH, "db.py")
+FAILURES_FILE_PATH = os.path.join(JOURNALS_FILE_PATH, "failures.py")
+
+
+def failure(thing_id: ThingId, failure_id: FailureId, ip: str, timestamp: int, comment: Optional[str]) -> None:
+    pass
+
+
+def failure_del(thing_id: ThingId, failure_id: FailureId, ip: str, timestamp: int) -> None:
+    pass
+
+def dispatch(
+    dispatch_id: DispatcherId,
+    failure_ids: list[FailureId],
+    action_id: ActionId,
+    group_id: UserId,
+    timestamp: int
+) -> None:
+    pass
 
 
 def load_config() -> None:
@@ -28,6 +50,22 @@ def load_config() -> None:
         "dispatcher_del": dispatcher_del,
     })
 
+def load_failures() -> None:
+    with open(FAILURES_FILE_PATH, 'r', encoding='utf-8') as f:
+        failures_file_contents = f.read()
+    failures_file_code = compile(failures_file_contents, FAILURES_FILE_PATH, 'exec')
+    exec(failures_file_code, {
+        "failure": failure,
+        "failure_del": failure_del,
+        "dispatch": dispatch,
+    })
+
 
 if __name__ == "__main__":
+    from qrbug.failure import Failure
+    from qrbug.user import User
+    print(Failure.instances)
+    print(User.instances)
     load_config()
+    print(Failure.instances)
+    print(User.instances)
