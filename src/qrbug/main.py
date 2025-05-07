2e2f78079215b0ef05ac16a404e8deb0ab4e3182
Mathieu <no@example.com>
2025-05-07 15:59:25 +0200

    Added current failures

--- src/qrbug/main.py
+++ src/qrbug/main.py
@@ -1,5 +1,6 @@
 import os
 from typing import Optional, Callable
+from dataclasses import dataclass
 
 from qrbug.user import user_add, user_remove, UserId
 from qrbug.failure import failure_update, failure_add, failure_remove, DisplayTypes, FailureId
@@ -8,17 +9,32 @@ from qrbug.action import action, ActionId
 from qrbug.selector import selector
 from qrbug.dispatcher import dispatcher_update, dispatcher_del, DispatcherId
 
+# Journal files
 JOURNALS_FILE_PATH = "JOURNALS/"
 DB_FILE_PATH = os.path.join(JOURNALS_FILE_PATH, "db.py")
 FAILURES_FILE_PATH = os.path.join(JOURNALS_FILE_PATH, "failures.py")
 
+# Failures remaining
+@dataclass
+class CurrentFailure:
+    thing_id: ThingId
+    failure_id: FailureId
+    ip: str
+    timestamp: int
+    comment: Optional[str] = None
+
+CURRENT_FAILURES: list[CurrentFailure] = []
+
 
 def failure(thing_id: ThingId, failure_id: FailureId, ip: str, timestamp: int, comment: Optional[str] = None) -> None:
-    pass
+    CURRENT_FAILURES.append(CurrentFailure(thing_id, failure_id, ip, timestamp, comment))
 
 
 def failure_del(thing_id: ThingId, failure_id: FailureId, ip: str, timestamp: int) -> None:
-    pass
+    for current_failure in CURRENT_FAILURES:
+        if current_failure.thing_id == thing_id and current_failure.failure_id == failure_id:
+            CURRENT_FAILURES.remove(current_failure)
+            break
 
 
 def dispatch(
