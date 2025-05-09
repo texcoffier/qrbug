c91ca76adc392f2899fbe5e2349d6e4175e71c3c
Mathieu <no@example.com>
2025-05-09 17:23:48 +0200

    Added the basis of the route that marks an incident as repaired

--- src/qrbug/server.py
+++ src/qrbug/server.py
@@ -1,11 +1,14 @@
 import asyncio
+import datetime
+import time
+from datetime import datetime
 from typing import Optional
 
 from aiohttp import web
 
 from qrbug.thing import Thing
 from qrbug.failure import Failure
-from qrbug.main import load_config, load_incidents
+from qrbug.main import load_config, load_incidents, INCIDENTS_FILE_PATH
 
 
 def get_failures(thing_id: str) -> str:
@@ -31,6 +34,51 @@ async def show_failures_tree_route(request: web.Request) -> web.Response:
     return web.Response(status=200, text=f"Thing ID: {thing_id}\n\n{requested_thing.dump()}\n\nFailures list :\n{get_failures(thing_id)}")
 
 
+async def register_incident(request: web.Request) -> web.Response:
+    thing_id: Optional[str] = request.rel_url.query.get("thing-id", None)
+    failure_id: Optional[str] = request.rel_url.query.get("failure-id", None)
+    is_repaired: Optional[str] = request.rel_url.query.get("is-repaired", None)
+    additional_info: Optional[str] = request.rel_url.query.get("additional-info", None)
+
+    valid_request = all((e is not None for e in (thing_id, failure_id, is_repaired)))
+    # TODO: Do better.
+    if valid_request is False:
+        missing_params = []
+        if thing_id is None:
+            missing_params.append("thing_id")
+        if failure_id is None:
+            missing_params.append("failure_id")
+        if is_repaired is None:
+            missing_params.append("is_repaired")
+        return web.Response(status=404, text=f"Missing query parameters : " + ", ".join(missing_params))
+
+    is_repaired_bool: bool = is_repaired == '1'
+    timestamp = int(time.time())
+    current_date = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+    function_name = "incident_del" if is_repaired_bool else "incident"
+    user_ip = request.remote
+
+    # TODO: ESCAPE ALL SINGLE QUOTES !
+    function_to_log = f"{function_name}('{thing_id}', '{failure_id}', '{user_ip}', {timestamp}"
+    if additional_info is not None and is_repaired_bool is False:
+        # incident_del() does not take additional_info as parameter,
+        # therefore if the incident is repaired, we must ABSOLUTELY NOT get into this if block
+        function_to_log += f", '{additional_info}'"
+    function_to_log += f")  # {current_date} TODO LOGIN\n"
+
+    with open(INCIDENTS_FILE_PATH, 'a', encoding='utf-8') as f:
+        f.write(function_to_log)
+
+    return_string = (f"thing_id={thing_id}\n"
+                     f"failure_id={failure_id}\n"
+                     f"is_repaired={is_repaired_bool}\n"
+                     f"additional_info={additional_info}\n\n"
+                     f"valid_request={valid_request}\n\n"
+                     f"Registered new incident.")
+    return web.Response(status=200, text=return_string)
+
+
+
 def init_server(argv = None) -> web.Application:
     if argv is None:
         argv = []
@@ -41,7 +89,10 @@ def init_server(argv = None) -> web.Application:
 
     # Creates the server
     app = web.Application()
-    app.add_routes([web.get('/{thing_id}', show_failures_tree_route)])
+    app.add_routes([
+        web.get('/{thing_id}', show_failures_tree_route),
+        web.get('/', register_incident)
+    ])
     return app
 
 if __name__ == "__main__":
