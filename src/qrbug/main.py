61fa81fe423283a7509a81f01044f3341d352c12
Mathieu <no@example.com>
2025-05-09 13:45:35 +0200

    Updated order of test in check() method for better output

--- src/qrbug/main.py
+++ src/qrbug/main.py
@@ -61,7 +61,9 @@ class TestCase(unittest.TestCase):
         Failure.instances.clear()
 
     def check(self, cls, value):
-        self.assertEqual('\n'.join(sorted(cls.dump_all())), value)
+        # The parameters are arranged in this order because the EXPECTED value goes first, followed by the
+        # actual value of the class dump
+        self.assertEqual(value, '\n'.join(sorted(cls.dump_all())))
 
     def load_config(self):
         test = sys.modules[self.__class__.__module__].__spec__.origin
