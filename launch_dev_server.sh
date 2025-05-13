31920da614d379f9fd2469d2fd08cf3bfac4fc97
Mathieu <no@example.com>
2025-05-13 15:18:03 +0200

    Commented script

--- launch_dev_server.sh
+++ launch_dev_server.sh
@@ -10,5 +10,9 @@ if [ -z "$2" ]; then
   LAUNCH_PORT="8080"
 fi
 
+# Launches a static dev server
 #python3 -m aiohttp.web -H "$LAUNCH_HOST" -P "$LAUNCH_PORT" qrbug.server:init_server
+
+# Command to launch a new dev server, that refreshes on file change
+# Requires aiohttp-devtools==1.1.2
 adev runserver src/qrbug/server.py --app-factory init_server --host "$LAUNCH_HOST" --port "$LAUNCH_PORT"
