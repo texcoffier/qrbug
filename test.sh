eee4998359740807758229cf4997b1aa484017ea
Mathieu <no@example.com>
2025-05-14 17:13:32 +0200

    Made tests more explicit

--- test.sh
+++ test.sh
@@ -13,18 +13,21 @@ sleep 1  # Required delay because of server startup time
 # Tests that you can load the page for a thing
 EXIT_CODE=0
 STATUS_CODE="$(curl -s -o /dev/null -w "%{http_code}" "http://127.0.0.1:8080/thing=test_thing")"
-echo "TEST: Access to failures list of test_thing returned HTTP code ${STATUS_CODE}"
+echo -n "TEST: Access to failures list of test_thing returned HTTP code ${STATUS_CODE}"
 if [ "$STATUS_CODE" -ne 200 ]; then
+  echo " -> FAIL"
   EXIT_CODE=1
+else
+  echo " -> OK"
 fi
 
 # Tests that you can register a failure
 curl -s -o /dev/null -w "" "http://127.0.0.1:8080/?thing-id=test_thing&failure-id=test&is-repaired=0"
 if [ -z "$(cat "TESTS/test_server_incidents.conf")" ]; then
-  echo "TEST: Failed to register the incident"
+  echo "TEST: Failed to register the incident -> FAIL"
   EXIT_CODE=2
 else
-  echo "TEST: Registered incident"
+  echo "TEST: Registered incident -> OK"
 fi
 
 # Kills the server
