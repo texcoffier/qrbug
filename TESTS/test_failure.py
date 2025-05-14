a6b77425dbffc22f9f20fd3e86ca2ae4d81147f9
Mathieu <no@example.com>
2025-05-14 12:49:15 +0200

    Updated tests for new local dumps

--- TESTS/test_failure.py
+++ TESTS/test_failure.py
@@ -6,12 +6,12 @@ class TestFailure(qrbug.main.TestCase):
         # Checks that the failures are actually registered upon creation
         self.check(qrbug.main.Failure, '')
         qrbug.main.failure_update('0')  # Creates a brand-new failure with ID zero
-        self.check(qrbug.main.Failure, f'0 [] ()')
+        self.check(qrbug.main.Failure, f'0 [] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None')
 
         # Checks that updating an attribute of the failure class works
         new_value = 'There is a new value'
         qrbug.main.failure_update('0', value=new_value)
-        self.check(qrbug.main.Failure, f"0 [] (val='{new_value}')")
+        self.check(qrbug.main.Failure, f"0 [] val:'{new_value}' type:text confirm:True group:None")
 
         # Checks that updating a non-existent attribute of the failure class DOESN'T work
         self.assertRaises(AssertionError, qrbug.main.failure_update, '0', non_existent_key='value')
@@ -27,13 +27,13 @@ class TestFailure(qrbug.main.TestCase):
         b = qrbug.main.Failure.get('1')
 
         # Checks that those two failures have no children
-        self.check(qrbug.main.Failure, f'0 [] ()\n'
-                            f'1 [] ()')
+        self.check(qrbug.main.Failure, f'0 [] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None\n'
+                            f'1 [] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None')
 
         # Parents 0 to 1 (adds 1 as the child of 0)
         qrbug.main.failure_add(a.id, b.id)
-        self.check(qrbug.main.Failure, f"0 ['1'] ()\n"
-                            f'1 [] ()')
+        self.check(qrbug.main.Failure, f"0 ['1'] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None\n"
+                            f'1 [] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None')
 
     def test_unparenting(self):
         self.assertEqual(len(qrbug.main.Failure.instances), 0)  # Checks that there are no failures created yet
@@ -45,11 +45,11 @@ class TestFailure(qrbug.main.TestCase):
 
         # Tests that both failures, if unparented, have no children anymore
         qrbug.main.failure_remove(a.id, b.id)
-        self.check(qrbug.main.Failure, f'0 [] ()\n'
-                            f'1 [] ()')
+        self.check(qrbug.main.Failure, f'0 [] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None\n'
+                            f'1 [] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None')
 
     def test_with_db(self):
         # Loads the DB where a simple failure is created
         self.load_config()
         # Checks that there are two users in the DB
-        self.check(qrbug.main.Failure, f"PC_NO_BOOT [] (confirm=False, group='ROOT', val='Le PC ne démarre pas')")
+        self.check(qrbug.main.Failure, f"PC_NO_BOOT [] val:'Le PC ne démarre pas' type:text confirm:False group:ROOT")
--- TESTS/test_thing.py
+++ TESTS/test_thing.py
@@ -7,7 +7,7 @@ class TestThing(qrbug.main.TestCase):
     def test_creation(self):
         self.check(qrbug.main.Thing, '')
         qrbug.main.thing_update(ID)
-        self.check(qrbug.main.Thing, f'{ID} [] ()')
+        self.check(qrbug.main.Thing, f'{ID} [] loc:None failure:None comment:\'\'')
 
     def test_update(self):
         # Creates a thing (without using the thing_update()) method
@@ -16,17 +16,17 @@ class TestThing(qrbug.main.TestCase):
         # Updates said thing
         new_value = 'THIS IS A NEW VALUE FOR THE COMMENT'
         qrbug.main.thing_update(ID, comment=new_value)
-        self.check(qrbug.main.Thing, f"{ID} [] (comment='{new_value}')")
+        self.check(qrbug.main.Thing, f"{ID} [] loc:None failure:None comment:'{new_value}'")
 
     def test_delete(self):
         # Creates a thing (without using the thing_update()) method
         qrbug.main.Thing.get(ID)
 
         # Tests around deleting an instance
-        self.check(qrbug.main.Thing, f'{ID} [] ()')
+        self.check(qrbug.main.Thing, f'{ID} [] loc:None failure:None comment:\'\'')
         qrbug.main.thing_del(ID)
         self.check(qrbug.main.Thing, '')
 
     def test_with_db(self):
         self.load_config()
-        self.check(qrbug.main.Thing, f"0 [] (comment='A comment, it seems')")
+        self.check(qrbug.main.Thing, f"0 [] loc:None failure:None comment:'A comment, it seems'")
