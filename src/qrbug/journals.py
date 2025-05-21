6f2d39a586aa447afb52234477e4d9d5ab8acaf0
Mathieu <no@example.com>
2025-05-21 17:48:17 +0200

    Added executors (circular imports, needs fix)

--- src/qrbug/journals.py
+++ src/qrbug/journals.py
@@ -5,6 +5,8 @@ from pathlib import Path
 from typing import Callable
 import enum
 
+import qrbug
+
 
 class Journals(enum.Enum):
     DB = enum.auto()
@@ -65,3 +67,57 @@ def append_line_to_journal(line: str, journal: Journals = Journals.INCIDENTS) ->
     line_vars = {}
     exec(compile('current_incident = ' + line, 'no file', 'exec'), given_globals, line_vars)
     return line_vars['current_incident']
+
+
+class Executor:
+    filename = None  # TODO: Default val
+    locals = {}
+    functions = {}
+
+    def __init__(self, filename: Path = None):
+        self.filename = filename or self.filename
+
+        self.before_load()
+        self.load()
+        self.after_load()
+
+    def before_load(self):
+        pass
+
+    def load(self):
+        self.locals = exec_code_file(self.filename, self.functions)
+
+    def after_load(self):
+        pass
+
+
+class Action(Executor):
+    import qrbug
+    functions = {"Incidents": qrbug.Incidents}
+
+
+class Journal(Executor):
+    def append(self, line: str):
+        with open(self.filename, 'a', encoding='utf-8') as f:
+            f.write(line)
+        line_vars = {}
+        exec(compile('value = ' + line, 'no file', 'exec'), self.functions, line_vars)
+        return line_vars['value']
+
+
+class JournalIncidents(Journal):
+    import qrbug
+    filename = qrbug.INCIDENTS_FILE_PATH
+    functions = qrbug.INCIDENT_FUNCTIONS
+
+
+class JournalDBReadOnly(Executor):
+    def before_load(self):
+        import qrbug
+        exec_code_file(qrbug.DEFAULT_DB_PATH, self.functions)
+        self.filename = qrbug.DB_FILE_PATH
+        self.functions = qrbug.CONFIGS
+
+
+class JournalDB(JournalDBReadOnly, Journal):
+    pass
