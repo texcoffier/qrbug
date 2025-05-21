e9868618ca721daddc39a850b1bbddaeeebda422
Mathieu <no@example.com>
2025-05-21 14:44:20 +0200

    Updated typing

--- ACTIONS/generate_qr.py
+++ ACTIONS/generate_qr.py
@@ -3,10 +3,11 @@ from typing import Optional
 import qrcode
 from aiohttp import web
 
-BASE_URL = 'https://qrbug.univ-lyon1.fr'
-REPORT_THING_URL = BASE_URL + '/thing={}'
+import qrbug
 
-def run(incident: "Incidents", _request: web.Request) -> Optional[str]:
+REPORT_THING_URL = qrbug.SERVICE_URL + '/thing={}'
+
+def run(incident: qrbug.Incidents, _request: web.Request) -> Optional[str]:
     url = REPORT_THING_URL.format(incident.thing_id)
     img = qrcode.make(url)
     img.show()
--- ACTIONS/none.py
+++ ACTIONS/none.py
@@ -4,5 +4,7 @@ An empty action : Does nothing.
 from typing import Optional
 from aiohttp import web
 
-def run(_incident: "Incidents", _request: web.Request) -> Optional[str]:
+import qrbug
+
+def run(_incident: qrbug.Incidents, _request: web.Request) -> Optional[str]:
     pass
