d15ee501b90c30e74ef00046c6050c30b82464ac
Mathieu <no@example.com>
2025-05-22 16:57:10 +0200

    Updated a bit of main

--- src/qrbug/main.py
+++ src/qrbug/main.py
@@ -2,14 +2,8 @@ import sys
 import unittest
 from pathlib import Path
 
-from qrbug.action import action, ActionId, Action
-from qrbug.dispatcher import dispatcher_update, dispatcher_del, DispatcherId, Dispatcher
-from qrbug.failure import failure_update, failure_add, failure_remove, DisplayTypes, FailureId, Failure
-from qrbug.incidents import incident, incident_del, Incidents
-from qrbug.selector import selector, Selector
-from qrbug.thing import thing_update, thing_del, Thing
-from qrbug.user import user_add, user_remove, UserId, User
-from qrbug.journals import exec_code_file
+import init
+import qrbug
 
 
 # SETTINGS
@@ -35,28 +29,28 @@ def set_incidents_path(path: Path) -> None:
 
 
 def dispatch(
-        dispatch_id: DispatcherId,
-        failure_ids: list[FailureId],
-        action_id: ActionId,
-        group_id: UserId,
+        dispatch_id: qrbug.DispatcherId,
+        failure_ids: list[qrbug.FailureId],
+        action_id: qrbug.ActionId,
+        group_id: qrbug.UserId,
         timestamp: int
 ) -> None:
-    dispatcher = Dispatcher[dispatch_id]
+    dispatcher = qrbug.Dispatcher[dispatch_id]
     if dispatcher is None:
         return
 
     # Looks for every incident with the given failure ids
     dispatched_incidents = []
     for failure_id in failure_ids:
-        for current_incident in Incidents.filter_active(failure_id=failure_id):
+        for current_incident in qrbug.Incidents.filter_active(failure_id=failure_id):
             dispatched_incidents.append(current_incident)
 
     dispatcher.run(dispatched_incidents, group_id, None)
 
 class TestCase(unittest.TestCase):
     def tearDown(self):
-        User.instances.clear()
-        Failure.instances.clear()
+        qrbug.User.instances.clear()
+        qrbug.Failure.instances.clear()
 
     def check(self, cls, value):
         # The parameters are arranged in this order because the EXPECTED value goes first, followed by the
@@ -65,27 +59,27 @@ class TestCase(unittest.TestCase):
 
     def load_config(self):
         test = sys.modules[self.__class__.__module__].__spec__.origin
-        exec_code_file(Path(test.replace('.py', '_db.conf')), CONFIGS)
+        qrbug.exec_code_file(Path(test.replace('.py', '_db.conf')), CONFIGS)
 
 
 CONFIGS = {
-    "user_add": user_add,
-    "user_remove": user_remove,
-    "failure_update": failure_update,
-    "failure_add": failure_add,
-    "failure_remove": failure_remove,
-    "DisplayTypes": DisplayTypes,
-    "thing_update": thing_update,
-    "thing_del": thing_del,
-    "action": action,
-    "selector": selector,
-    "dispatcher_update": dispatcher_update,
-    "dispatcher_del": dispatcher_del,
+    "user_add": qrbug.user_add,
+    "user_remove": qrbug.user_remove,
+    "failure_update": qrbug.failure_update,
+    "failure_add": qrbug.failure_add,
+    "failure_remove": qrbug.failure_remove,
+    "DisplayTypes": qrbug.DisplayTypes,
+    "thing_update": qrbug.thing_update,
+    "thing_del": qrbug.thing_del,
+    "action": qrbug.action,
+    "selector": qrbug.selector,
+    "dispatcher_update": qrbug.dispatcher_update,
+    "dispatcher_del": qrbug.dispatcher_del,
 }
 
 INCIDENT_FUNCTIONS = {
-    "incident": incident,
-    "incident_del": incident_del,
+    "incident": qrbug.incident,
+    "incident_del": qrbug.incident_del,
     "dispatch": dispatch,
 }
 
@@ -103,14 +97,14 @@ QRBUG = {
     **CONFIGS,
     **INCIDENT_FUNCTIONS,
     **SETTINGS,
-    "User": User,
-    "Failure": Failure,
-    "Thing": Thing,
-    "Action": Action,
-    "Selector": Selector,
-    "Dispatcher": Dispatcher,
-    "Incidents": Incidents,
-    "exec_code_file": exec_code_file,
+    "User": qrbug.User,
+    "Failure": qrbug.Failure,
+    "Thing": qrbug.Thing,
+    "Action": qrbug.Action,
+    "Selector": qrbug.Selector,
+    "Dispatcher": qrbug.Dispatcher,
+    "Incidents": qrbug.Incidents,
+    "exec_code_file": qrbug.exec_code_file,
     "CONFIGS": CONFIGS,
     "INCIDENT_FUNCTIONS": INCIDENT_FUNCTIONS,
     "TestCase": TestCase,
