c34925c12b6a0e3c67e3188b5102589c474cc977
Mathieu <nope@example.com>
2025-05-27 17:20:22 +0200

    Can now display separate journals

--- ACTIONS/show_journals.py
+++ ACTIONS/show_journals.py
@@ -12,12 +12,20 @@ async def run(incident: qrbug.Incidents, _request: web.Request) -> Optional[str]
     async def get_html_from_path(path: Path):
         return path.read_text(encoding='utf-8')
 
-    if incident.failure_id == 'SHOW_JOURNALS':
-        db_file_contents = await get_html_from_path(qrbug.DB_FILE_PATH)
-        default_db_file_contents = await get_html_from_path(qrbug.DEFAULT_DB_PATH)
-        incidents_file_contents = await get_html_from_path(qrbug.INCIDENTS_FILE_PATH)
-        return ( '<style>pre { font-family: monospace, monospace; background: #FFC; }</style>\n\n'
-                f'<h2>Default DB :</h2><pre>{default_db_file_contents}</pre>\n'
-                f'<h2>DB :</h2><pre>{db_file_contents}</pre>\n'
-                f'<h2>Incidents:</h2><pre>{incidents_file_contents}</pre>')
+    if incident.failure_id.startswith('SHOW_JOURNALS'):
+        failure_id = incident.failure_id.removeprefix('SHOW_JOURNALS')
+        failure_id = failure_id.removeprefix('_')  # The majority of 'SHOW_JOURNALS' failures take the shape of 'SHOW_JOURNALS_*', so removing the underscore is necessary
+
+        final_string: list[str] = ['<style>pre { font-family: monospace, monospace; background: #FFC; }</style>\n\n']
+
+        def write(name: str, string: str):
+            final_string.append(f'<h2>{name} :</h2><pre>{string}</pre>\n')
+
+        if failure_id in ('', 'ALL', 'DB'):
+            write('DB', await get_html_from_path(qrbug.DB_FILE_PATH))
+        if failure_id in ('', 'ALL', 'DEFAULT_DB'):
+            write('Default DB', await get_html_from_path(qrbug.DEFAULT_DB_PATH))
+        if failure_id in ('', 'ALL', 'INCIDENTS'):
+            write('Incidents', await get_html_from_path(qrbug.INCIDENTS_FILE_PATH))
+        return ''.join(final_string)
     return None
