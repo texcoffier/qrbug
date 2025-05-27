30840844645dec0732c00a49cf4e59b318835c67
Mathieu <nope@example.com>
2025-05-27 17:07:11 +0200

    Asynchronized a bit of the code

--- ACTIONS/show_journals.py
+++ ACTIONS/show_journals.py
@@ -9,13 +9,13 @@ async def run(incident: qrbug.Incidents, _request: web.Request) -> Optional[str]
     import qrbug
 
 
-    def get_html_from_path(path: Path):
+    async def get_html_from_path(path: Path):
         return path.read_text(encoding='utf-8')
 
     if incident.failure_id == 'SHOW_JOURNALS':
-        db_file_contents = get_html_from_path(qrbug.DB_FILE_PATH)
-        default_db_file_contents = get_html_from_path(qrbug.DEFAULT_DB_PATH)
-        incidents_file_contents = get_html_from_path(qrbug.INCIDENTS_FILE_PATH)
+        db_file_contents = await get_html_from_path(qrbug.DB_FILE_PATH)
+        default_db_file_contents = await get_html_from_path(qrbug.DEFAULT_DB_PATH)
+        incidents_file_contents = await get_html_from_path(qrbug.INCIDENTS_FILE_PATH)
         return ( '<style>pre { font-family: monospace, monospace; background: #FFC; }</style>\n\n'
                 f'<h2>Default DB :</h2><pre>{default_db_file_contents}</pre>\n'
                 f'<h2>DB :</h2><pre>{db_file_contents}</pre>\n'
