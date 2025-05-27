bfb9b63e04471c4ec413d82cbe6e6f395503c4f8
Mathieu <nope@example.com>
2025-05-27 17:04:33 +0200

    Stylized the content of the journals

--- ACTIONS/show_journals.py
+++ ACTIONS/show_journals.py
@@ -10,13 +10,14 @@ async def run(incident: qrbug.Incidents, _request: web.Request) -> Optional[str]
 
 
     def get_html_from_path(path: Path):
-        return path.read_text(encoding='utf-8').replace('\n', '<br/>\n')
+        return path.read_text(encoding='utf-8')
 
     if incident.failure_id == 'SHOW_JOURNALS':
         db_file_contents = get_html_from_path(qrbug.DB_FILE_PATH)
         default_db_file_contents = get_html_from_path(qrbug.DEFAULT_DB_PATH)
         incidents_file_contents = get_html_from_path(qrbug.INCIDENTS_FILE_PATH)
-        return (f'<h2>Default DB :</h2><div class="code">{default_db_file_contents}</div>\n'
-                f'<h2>DB :</h2><div class="code">{db_file_contents}</div>\n'
-                f'<h2>Incidents:</h2><div class="code">{incidents_file_contents}</div>')
+        return ( '<style>pre { font-family: monospace, monospace; background: #FFC; }</style>\n\n'
+                f'<h2>Default DB :</h2><pre>{default_db_file_contents}</pre>\n'
+                f'<h2>DB :</h2><pre>{db_file_contents}</pre>\n'
+                f'<h2>Incidents:</h2><pre>{incidents_file_contents}</pre>')
     return None
