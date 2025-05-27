7b79df8cfb37c65366771e11a35903edd426c625
Mathieu <nope@example.com>
2025-05-27 17:44:20 +0200

    Show_journals now streams the response

--- ACTIONS/show_journals.py
+++ ACTIONS/show_journals.py
@@ -4,28 +4,34 @@ from aiohttp import web
 import qrbug
 
 
-async def run(incident: qrbug.Incidents, _request: web.Request) -> Optional[str]:
+# TODO: Make this less XSS friendly
+# TODO: Stream the answer
+
+# TODO: Clore l'incident qui a été créé pour ce DEBUG -> Rajouter un attribut 'auto-close' à failure, qui met que la failure est réparée tout de suite
+# TODO:                                               -> **OU** faire en sorte que le dispatcher répare la failure, avec le 'auto-close' spécifié dans le dispatcher
+async def run(incident: qrbug.Incidents, request: web.Request) -> Optional[str]:
     from pathlib import Path
     import qrbug
 
 
     async def get_html_from_path(path: Path):
-        return path.read_text(encoding='utf-8')
+        return path.read_text(encoding='utf-8')  # TODO: Lire 100Ko par 100Ko le fichier puis asyncio.sleep(0)
 
     if incident.failure_id.startswith('SHOW_JOURNALS'):
         failure_id = incident.failure_id.removeprefix('SHOW_JOURNALS')
         failure_id = failure_id.removeprefix('_')  # The majority of 'SHOW_JOURNALS' failures take the shape of 'SHOW_JOURNALS_*', so removing the underscore is necessary
 
-        final_string: list[str] = ['<style>pre { font-family: monospace, monospace; background: #FFC; }</style>\n\n']
+        await request.response.write(
+            '<style>pre { font-family: monospace, monospace; background: #FFC; }</style>\n\n'.encode('utf-8')
+        )
 
-        def write(name: str, string: str):
-            final_string.append(f'<h2>{name} :</h2><pre>{string}</pre>\n')
+        async def write(name: str, string: str):
+            await request.response.write(f'<h2>{name} :</h2><pre>{string}</pre>\n'.encode('utf-8'))
 
         if failure_id in ('', 'ALL', 'DB'):
-            write('DB', await get_html_from_path(qrbug.DB_FILE_PATH))
+            await write('DB', await get_html_from_path(qrbug.DB_FILE_PATH))
         if failure_id in ('', 'ALL', 'DEFAULT_DB'):
-            write('Default DB', await get_html_from_path(qrbug.DEFAULT_DB_PATH))
+            await write('Default DB', await get_html_from_path(qrbug.DEFAULT_DB_PATH))
         if failure_id in ('', 'ALL', 'INCIDENTS'):
-            write('Incidents', await get_html_from_path(qrbug.INCIDENTS_FILE_PATH))
-        return ''.join(final_string)
+            await write('Incidents', await get_html_from_path(qrbug.INCIDENTS_FILE_PATH))
     return None
