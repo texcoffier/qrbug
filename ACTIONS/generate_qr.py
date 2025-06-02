3c2eaa6bbddf89e33e3e83ac16b16a79196df54b
Mathieu <nope@example.com>
2025-06-02 19:30:19 +0200

    Added proper QR code generation

--- ACTIONS/generate_qr.py
+++ ACTIONS/generate_qr.py
@@ -1,13 +1,30 @@
 from typing import Optional
-
-import qrcode
 from aiohttp import web
 
 import qrbug
 
-REPORT_THING_URL = qrbug.SERVICE_URL + '/thing={}'
+async def run(incident: qrbug.Incident, request: web.Request) -> Optional[str]:
+    import base64
+    from io import BytesIO
+    import qrbug
+    import qrcode
 
-def run(incident: qrbug.Incident, _request: web.Request) -> Optional[str]:
-    url = REPORT_THING_URL.format(incident.thing_id)
+    REPORT_THING_URL = qrbug.SERVICE_URL + '/thing={}'
+
+    requested_thing_id = incident.comment
+    url = REPORT_THING_URL.format(requested_thing_id)
+
     img = qrcode.make(url)
-    img.show()
+
+    buffer = BytesIO()
+    img.save(buffer, format='JPEG')
+    img_base64 = base64.b64encode(buffer.getvalue())
+
+    # Writes the HTML of the QR code
+    await request.response.write((
+        f'<h2>QR Code for thing {requested_thing_id}</h2>'
+        f'<div><img src="data:image/jpeg;base64,{img_base64.decode()}" /></div>'
+    ).encode('utf-8'))
+
+    # TODO: Auto-close
+    pass
