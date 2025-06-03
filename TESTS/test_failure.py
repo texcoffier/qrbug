661433ba44e8040d54f21677be67504ad5efe4a0
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2025-06-03 09:23:40 +0200

    Fix regression tests

--- TESTS/test_failure.py
+++ TESTS/test_failure.py
@@ -7,7 +7,7 @@ class TestFailure(qrbug.TestCase):
         # Checks that the failures are actually registered upon creation
         self.check(qrbug.Failure, '')
         qrbug.failure_update('0')  # Creates a brand-new failure with ID zero
-        self.check(qrbug.Failure, f'0 [] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None')
+        self.check(qrbug.Failure, f'0 [] val:\'VALEUR_NON_DEFINIE POUR «0»\' type:text confirm:True group:None')
 
         # Checks that updating an attribute of the failure class works
         new_value = 'There is a new value'
@@ -28,13 +28,13 @@ class TestFailure(qrbug.TestCase):
         b = qrbug.Failure.get('1')
 
         # Checks that those two failures have no children
-        self.check(qrbug.Failure, f'0 [] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None\n'
-                            f'1 [] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None')
+        self.check(qrbug.Failure, f'0 [] val:\'VALEUR_NON_DEFINIE POUR «0»\' type:text confirm:True group:None\n'
+                            f'1 [] val:\'VALEUR_NON_DEFINIE POUR «1»\' type:text confirm:True group:None')
 
         # Parents 0 to 1 (adds 1 as the child of 0)
         qrbug.failure_add(a.id, b.id)
-        self.check(qrbug.Failure, f"0 ['1'] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None\n"
-                            f'1 [] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None')
+        self.check(qrbug.Failure, f"0 ['1'] val:\'VALEUR_NON_DEFINIE POUR «0»\' type:text confirm:True group:None\n"
+                            f'1 [] val:\'VALEUR_NON_DEFINIE POUR «1»\' type:text confirm:True group:None')
 
     def test_unparenting(self):
         self.assertEqual(len(qrbug.Failure.instances), 0)  # Checks that there are no failures created yet
@@ -46,8 +46,8 @@ class TestFailure(qrbug.TestCase):
 
         # Tests that both failures, if unparented, have no children anymore
         qrbug.failure_remove(a.id, b.id)
-        self.check(qrbug.Failure, f'0 [] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None\n'
-                            f'1 [] val:\'VALEUR_NON_DEFINIE\' type:text confirm:True group:None')
+        self.check(qrbug.Failure, f'0 [] val:\'VALEUR_NON_DEFINIE POUR «0»\' type:text confirm:True group:None\n'
+                            f'1 [] val:\'VALEUR_NON_DEFINIE POUR «1»\' type:text confirm:True group:None')
 
     def test_with_db(self):
         # Loads the DB where a simple failure is created
