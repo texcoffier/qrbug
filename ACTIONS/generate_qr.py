39d076983c7caebcb01ad360a23e324942b11431
Mathieu <no@example.com>
2025-06-12 17:07:06 +0200

    Templated QR block

--- ACTIONS/generate_qr.py
+++ ACTIONS/generate_qr.py
@@ -12,6 +12,15 @@ REPORT_THING_URL = qrbug.SERVICE_URL + '/thing={}'
 QR_GEN_THING_ID = 'QR_GEN'
 QR_GEN_FAILURE_ID = 'generate_qr'
 
+TEMPLATE_QR_BLOCK = """
+<div class="qr_block">
+    <h2>QR Code pour <a href="{qr_link}">{thing_id}</a></h2>
+    <div>
+        <img src="data:image/{img_format};base64,{img_b64}" />
+    </div>
+</div>
+"""
+
 
 def get_qr_gen_link(thing_id: qrbug.ThingId, ticket: str) -> str:
     return f'/?thing-id={QR_GEN_THING_ID}&failure-id={QR_GEN_FAILURE_ID}&what=thing&is-repaired=0&&additional-info={thing_id}&ticket={ticket}'
@@ -30,6 +39,8 @@ async def run(incidents: list[qrbug.Incident], request: qrbug.Request) -> Option
     if not requested_thing:
         return qrbug.action_helpers.ActionReturnValue(error_msg=f"Thing {repr(requested_thing_id)} not found")
 
+    user_ticket = request.query.get("ticket", "")
+
     for thing_id in [requested_thing_id, *requested_thing.get_all_children_ids()]:
         url = REPORT_THING_URL.format(thing_id)
 
@@ -43,12 +54,12 @@ async def run(incidents: list[qrbug.Incident], request: qrbug.Request) -> Option
         # sorted(Thing[id] for id in get_all_children_ids(), key=lambda x: x.path())
 
         # Writes the HTML of the QR code
-        await request.write(
-            f'<div class="qr_block">'
-            f'<h2>QR Code pour <a href="{get_qr_gen_link(thing_id, request.query.get("ticket", ""))}">{thing_id}</a></h2>'
-            f'<div><img src="data:image/{IMAGE_FORMAT.lower()};base64,{img_base64.decode()}" /></div>'
-            f'</div>'
-        )
+        await request.write(TEMPLATE_QR_BLOCK.format(
+            qr_link    = get_qr_gen_link(thing_id, user_ticket),
+            thing_id   = thing_id,
+            img_format = IMAGE_FORMAT.lower(),
+            img_b64    = img_base64.decode(),
+        ))
 
     await request.write(
         '<h3>Parents :</h3>'
@@ -56,7 +67,7 @@ async def run(incidents: list[qrbug.Incident], request: qrbug.Request) -> Option
     )
     for parent_id in requested_thing.parent_ids:
         await request.write(
-            f'<li><a href="{get_qr_gen_link(parent_id, request.query.get("ticket", ""))}">{parent_id}</a></li>'
+            f'<li><a href="{get_qr_gen_link(parent_id, user_ticket)}">{parent_id}</a></li>'
         )
     await request.write('</ul>')
 
