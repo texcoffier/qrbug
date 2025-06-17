2a25ff6583ef6a22fd534f067302784590184386
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2025-06-17 07:57:58 +0200

    Test server starting speed with a huge journal

---
+++ test-load.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python3
+"""
+Create a big config and incident journal
+"""
+
+things = []
+with open('TESTS/xxx-testload.conf', 'w') as file:
+    file.write(
+        'user_update("ROOT")\n'
+        'user_add("admin", "ROOT")\n'
+        'thing_update("Campus")\n'
+        'failure_update("F1")\n'
+        'failure_update("F2")\n'
+        'failure_update("F3")\n'
+        )
+    for b in range(30):
+        building = f'"Building{b}"'
+        file.write(
+            f'thing_update({building})\n'
+            f'thing_add("Campus",{building})\n'
+        )
+        for r in range(30):
+            room = f'"Room{b}_{r}"'
+            file.write(
+                f'thing_update({room})\n'
+                f'thing_add({building}, {room})\n'
+            )
+            for host in range(40):
+                host = f'"Host{b}_{r}_{host}"'
+                file.write(
+                    f'thing_update({host})\n'
+                    f'thing_add({room}, {host})\n'
+                    f'thing_add_failure({host}, "F")\n'
+                    )
+                things.append(host)
+
+with open('TESTS/xxx-incidents.py', 'w') as file:
+    for thing in things:
+        for failure in ('F1', 'F2', 'F3'):
+            file.write(f"""
+
+# User report
+incident_new('{thing}', '{failure}', '127.0.0.1', 1749979950, '', '') # 2025-06-15 11:32:30
+
+# Admin check if something to do
+incident_new('admin', 'personnal-for-me', '127.0.0.1', 1749979935, '', 'thierry.excoffier') # 2025-06-15 11:32:15
+
+# Display the incident list
+dispatch('personnal-for-me', 'personnal-for-me', 'admin', 'echo', 1749979935) # 2025-06-15 11:32:15 1 incidents
+
+# Close the API incident
+incident_del('admin', 'personnal-for-me', '127.0.0.1', 1749979935, 'thierry.excoffier') # 2025-06-15 11:32:15
+
+# Cleanup needed feedback
+dispatch('z-backoffice-close', 'personnal-for-me', 'admin', 'close_auto', 1749979935) # 2025-06-15 11:32:15 1 incidents
+
+# Mark incident as fixed
+incident_del('{thing}', '{failure}', '127.0.0.1', 1749979935, 'thierry.excoffier') # 2025-06-15 11:32:15
+
+# List waiting feedback
+incident_new('admin', 'pending-feedback', '127.0.0.1', 1750086416, '', 'thierry.excoffier') # 2025-06-16 17:06:56
+dispatch('pending-feedback', 'pending-feedback', 'admin', 'echo', 1750086416) # 2025-06-16 17:06:56 1 incidents
+incident_del('admin', 'pending-feedback', '127.0.0.1', 1750086416, 'thierry.excoffier') # 2025-06-16 17:06:56
+dispatch('z-backoffice-close', 'pending-feedback', 'admin', 'close_auto', 1750086416) # 2025-06-16 17:06:56 1 incidents
+
+# Send user feedback (erase list in memory)
+incident_new('admin', 'send-pending-feedback', '127.0.0.1', 1750086503, '', 'thierry.excoffier') # 2025-06-16 17:08:23
+dispatch('send-pending-feedback', 'send-pending-feedback', 'admin', 'pending_feedback', 1750086503) # 2025-06-16 17:08:23 1 incidents
+incident_del('admin', 'send-pending-feedback', '127.0.0.1', 1750086503, 'thierry.excoffier') # 2025-06-16 17:08:23
+dispatch('z-backoffice-close', 'send-pending-feedback', 'admin', 'close_auto', 1750086503) # 2025-06-16 17:08:23 1 incidents
+
+""")
+
+
+
---
+++ test-load.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# 50MB RAM for a config with 10000 objects
+
+. .venv/bin/activate
+
+PROFILE="--profile"
+PROFILE=
+
+echo "Create big journals"
+./test-load.py # Create big config journal an incident journal
+ls -ls TESTS/xxx-testload.conf TESTS/xxx-incidents.py
+
+echo "Start server. WAIT A MINUTE."
+python3 src/qrbug/server.py "localhost" "8080" --test --testload $PROFILE >TESTS/xxx-load-server.log 2>&1 &
+QRBUG_PID=$!
+trap "kill $QRBUG_PID" 0 # Kill server on exit
+
+START=$(date "+%s")
+OK='0'
+for _ in $(seq 10000)
+do
+  if [ "$(curl -s -o TESTS/xxx-page-content -w "%{http_code}" "http://localhost:8080/?what=thing&thing-id=admin&failure-id=stats")" = 200 ]; then
+    OK='1'
+    break
+  else
+    sleep 1
+  fi
+done
+if [ "$OK" = '0' ]; then
+  echo "src/qrbug/server.py cannot be started"
+  exit 1
+fi
+echo "Server running after $(expr $(date '+%s') - $START) seconds"
+ps -fle | grep -e UID -e $QRBUG_PID
+
+echo "Profiling information in : TESTS/xxx-load-server.log"
+
+if grep -q '0 tickets sans retour' TESTS/xxx-page-content
+then
+  echo " -> content OK"
+else
+  echo " -> content is BAD, current content is in TESTS/xxx-page-content"
+  EXIT_CODE=1
+fi
+
+kill $QRBUG_PID
