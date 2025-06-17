470b76de1bb818ae6d44abb3a8157505e89dbf50
Mathieu <no@example.com>
2025-06-17 15:57:44 +0200

    Can now get all children of a Tree instance, sorted by level

--- src/qrbug/tree.py
+++ src/qrbug/tree.py
@@ -67,6 +67,17 @@ class Tree(qrbug.Editable):
         add_child_to_set(self)
         return all_children
 
+    def get_sorted_children_ids(self, max_depth: int = -1) -> list[str]:
+        def add_child_to_list(node, depth: int = 0):
+            if depth >= max_depth and max_depth != -1:
+                return
+            for child_id in sorted(node.children_ids):
+                all_children.append(child_id)
+                add_child_to_list(self.instances[child_id], depth + 1)
+        all_children = [self.id]
+        add_child_to_list(self)
+        return all_children
+
     def walk(self, go_in, go_out):
         done = set()
         def walk_(node):
