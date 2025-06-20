3458161c0882eb58cb206a6b1eef9b932783d882
Mathieu <nope@example.com>
2025-06-20 17:12:29 +0200

    Prettified edit_dispatcher output

--- ACTIONS/edit_dispatcher.py
+++ ACTIONS/edit_dispatcher.py
@@ -3,6 +3,9 @@ In this case, the thing is a dispatcher ID to edit
 """
 import qrbug
 
+BEACON_NAME = 'pre'
+FULL_BEACON = f'<{BEACON_NAME} style="display: inline;">'
+
 async def run(incidents, request):
     incident = incidents[0]
     dispatcher_id = incident.thing_id
@@ -10,11 +13,11 @@ async def run(incidents, request):
     if incident.failure_id == 'dispatcher-action_id':
         qrbug.append_line_to_journal(
                 f'dispatcher_update({repr(dispatcher_id)}, action_id={repr(value)})\n', qrbug.Journals.DB)
-        feedback = f"Le nouvel <i>action_id</i> du dispatcher {dispatcher_id} est {value}.\n"
+        feedback = f"Le nouvel {FULL_BEACON}action_id</{BEACON_NAME}> du dispatcher {FULL_BEACON}{dispatcher_id}</{BEACON_NAME}> est {FULL_BEACON}{value}{FULL_BEACON}.\n"
     elif incident.failure_id == 'dispatcher-selector_id':
         qrbug.append_line_to_journal(
                 f'dispatcher_update({repr(dispatcher_id)}, selector_id={repr(value)})\n', qrbug.Journals.DB)
-        feedback = f"Le nouveeau <i>selector_id</i> du dispatcher {dispatcher_id} est {value}.\n"
+        feedback = f"Le nouveeau {FULL_BEACON}selector_id</{BEACON_NAME}> du dispatcher {FULL_BEACON}{dispatcher_id}</{BEACON_NAME}> est {FULL_BEACON}{value}</{BEACON_NAME}>.\n"
     else:
         feedback = "Unexpected edit failure for Dispatcher\n"
     await request.write('<!DOCTYPE html>\n' + feedback)
