281cd1c31a0540132aaeccdf53065459ab196ec7
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2025-06-21 16:06:04 +0200

    Logging system: shorter names

--- src/qrbug/logging_system.py
+++ src/qrbug/logging_system.py
@@ -5,33 +5,27 @@ from typing import Union
 
 import qrbug
 
-
-def log(log_folder_path: Path, log_file_prefix: str, content: Union[str, bytes]):
+def log(log_folder_path: Path, content: Union[str, bytes]):
     """
     Logs the given content to a log file.
     """
-    current_time = time.strftime("%Y-%m-%d-%H-%M-%S")
-    filename = log_folder_path / f'{log_file_prefix}-{current_time}.log'
-    attempts_count = 1
+    current_time = time.strftime("%Y%m%d%H%M%S")
+    filename = log_folder_path / current_time
+    attempts_count = 0
     while os.path.exists(filename):
-        filename = log_folder_path / f'{log_file_prefix}-{current_time} ({attempts_count + 1}).log'
         attempts_count += 1
+        filename = log_folder_path / f'{current_time}-{attempts_count}'
 
     if isinstance(content, bytes):
-        params = {'mode': 'wb'}
+        filename.write_bytes(content)
     else:
-        params = {'mode': 'w', 'encoding':'utf-8'}
-    with open(filename, **params) as f:
-        f.write(content)
-
+        filename.write(content, encoding='utf-8')
 
 def log_email(content: bytes):
-    return log(qrbug.EMAIL_LOG_DIRECTORY, qrbug.EMAIL_LOG_PREFIX, content)
-
+    return log(qrbug.EMAIL_LOG_DIRECTORY, content)
 
 def log_error(content: str):
-    return log(qrbug.ERROR_LOG_DIRECTORY, qrbug.ERROR_LOG_PREFIX, content)
-
+    return log(qrbug.ERROR_LOG_DIRECTORY, content)
 
 qrbug.log = log
 qrbug.log_email = log_email
