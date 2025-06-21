53b65bb72574d08ff94ac2a7bb20dace594bb438
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2025-06-21 11:19:52 +0200

    Add missings 'secret'

--- ACTIONS/list.py
+++ ACTIONS/list.py
@@ -3,23 +3,23 @@ from typing import Optional, List
 
 import qrbug
 
-def link_to_object(what, thing_id, label=None):
+def link_to_object(what, thing_id, request, label=None):
     thing_id = html.escape(thing_id)
     if label is None:
         label = thing_id
-    return f'<a target="_blank" href="{what}={thing_id}">{label}</a>'
+    return f'<a target="_blank" href="{what}={thing_id}?secret={request.secret.secret}">{label}</a>'
 
-def link_to_active(thing_id):
+def link_to_active(thing_id, request):
     active = sum(len(i.active) for i in qrbug.Incident.instances[thing_id].values())
     if not active:
         return ''
-    return f'<a target="_blank" href="?failure-id=thing-incidents-active&thing-id={thing_id}">{active}</a>'
+    return f'<a target="_blank" href="?failure-id=thing-incidents-active&thing-id={thing_id}&secret={request.secret.secret}">{active}</a>'
 
-def link_to_finished(thing_id):
+def link_to_finished(thing_id, request):
     finished = sum(len(i.finished) for i in qrbug.Incident.instances[thing_id].values())
     if not finished:
         return ''
-    return f'<a target="_blank" href="?failure-id=thing-incidents&thing-id={thing_id}">{finished}</a>'
+    return f'<a target="_blank" href="?failure-id=thing-incidents&thing-id={thing_id}&secret={request.secret.secret}">{finished}</a>'
 
 async def run(incidents: List[qrbug.Incident], request: qrbug.Request) -> Optional[qrbug.action_helpers.ActionReturnValue]:
     incident = incidents[0]
@@ -30,12 +30,12 @@ async def run(incidents: List[qrbug.Incident], request: qrbug.Request) -> Option
     if issubclass(what, qrbug.Tree):
         if what is qrbug.Thing:
             thing_comment = qrbug.Failure['thing-comment']
-            texts.append(f'<p>Générer une feuille de QR codes : ')
+            texts.append('<p>Générer une feuille de QR codes : ')
             texts.append(' '.join(
                 f'<button onclick="qr(this)">{html.escape(failure.split("_")[-1])}</button>'
                 for failure in qrbug.Failure['generate_qr'].children_ids
             ))
-            texts.append(f'<p id="qr_code_gen_error_field"></p></p>')
+            texts.append('<p id="qr_code_gen_error_field"></p></p>')
             texts.append('<table>')
             texts.append('<tr><th> <th>Objet<th>')
             texts.append(html.escape(thing_comment.value))
@@ -45,15 +45,15 @@ async def run(incidents: List[qrbug.Incident], request: qrbug.Request) -> Option
                 texts.append(f'<input type="checkbox" class="qr_thing_checkboxes" id="qr_thing_checkbox_{node.id}" onclick="qr_select(this.checked, {repr(node.id)});" />')
                 texts.append('<td>')
                 texts.append(go_in.indent)
-                texts.append(link_to_object('thing', node.id))
+                texts.append(link_to_object('thing', node.id, request))
                 texts.append('<td>')
                 texts.append(qrbug.element(thing_comment, node, in_place=True))
                 texts.append('<td>')
                 thing_incident = qrbug.Incident.instances.get(node.id, None)
                 if thing_incident:
-                    texts.append(link_to_active(node.id))
+                    texts.append(link_to_active(node.id, request))
                     texts.append('<td>')
-                    texts.append(link_to_finished(node.id))
+                    texts.append(link_to_finished(node.id, request))
                 else:
                     texts.append('<td>')
                 texts.append('</tr>')
@@ -72,7 +72,7 @@ async def run(incidents: List[qrbug.Incident], request: qrbug.Request) -> Option
             def go_in(node):
                 texts.append('<tr><td>')
                 texts.append(go_in.indent)
-                texts.append(link_to_object('failure', node.id))
+                texts.append(link_to_object('failure', node.id, request))
                 texts.append('<td>')
                 texts.append(qrbug.element(failure_value, node, in_place=True))
                 texts.append('<td>')
@@ -163,7 +163,7 @@ async def run(incidents: List[qrbug.Incident], request: qrbug.Request) -> Option
         texts.append('</th></tr>')
         for node in sorted(what.instances.values(), key=lambda e: e.id):
             texts.append('<tr><td>')
-            texts.append(link_to_object('action', node.id))
+            texts.append(link_to_object('action', node.id, request))
             texts.append('<td>')
             texts.append(qrbug.element(action, node, in_place=True))
             texts.append('</tr>')
@@ -173,12 +173,12 @@ async def run(incidents: List[qrbug.Incident], request: qrbug.Request) -> Option
         texts.append('<table border><tr><th>Objet<th>Actives<th>Réparés<th>Panne<th>Active<th>Réparées')
         for thing_id, failures in sorted(what.instances.items()):
             texts.append(f'''<tr><td rowspan="{len(failures)}">
-            {link_to_object("thing", thing_id)}
-            <td rowspan="{len(failures)}">{link_to_active(thing_id)}
-            <td rowspan="{len(failures)}">{link_to_finished(thing_id) or "0"}''')
+            {link_to_object("thing", thing_id, request)}
+            <td rowspan="{len(failures)}">{link_to_active(thing_id, request)}
+            <td rowspan="{len(failures)}">{link_to_finished(thing_id, request) or "0"}''')
             prefix = ''
             for failure_id, failure in failures.items():
-                texts.append(f'''{prefix}<td>{link_to_object("failure", failure_id)}
+                texts.append(f'''{prefix}<td>{link_to_object("failure", failure_id, request)}
                 <td>{len(failure.active)}
                 <td>{len(failure.finished)}
                 </tr>
