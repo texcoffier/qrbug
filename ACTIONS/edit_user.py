aff7a0628e35bca048cdf02c3ed307f02a350ec6
Mathieu <no@example.com>
2025-06-25 17:23:29 +0200

    Improved error handling of user children

--- ACTIONS/edit_user.py
+++ ACTIONS/edit_user.py
@@ -10,11 +10,21 @@ async def run(incidents, request):
     user_id = incident.thing_id
     value = html.escape(request.report.comment)
     if incident.failure_id == 'user-add-child':
-        qrbug.append_line_to_journal(f'user_add({repr(user_id)}, {repr(value)})\n', qrbug.Journals.DB)
-        feedback = f"Ajouté l'enfant «{value}» à «{user_id}»\n"
+        if value == user_id:
+            feedback = f"<b>ERREUR :</b> Impossible d'assigner le même ID à un parent et un enfant"
+        elif value in qrbug.User[user_id].get_all_children_ids():
+            feedback = f"<b>ERREUR :</b> «{value}» est déjà un enfant de «{user_id}»"
+        else:
+            qrbug.append_line_to_journal(f'user_add({repr(user_id)}, {repr(value)})\n', qrbug.Journals.DB)
+            feedback = f"Ajouté l'enfant «{value}» à «{user_id}»\n"
     elif incident.failure_id == 'user-del-child':
-        qrbug.append_line_to_journal(f'user_remove({repr(user_id)}, {repr(value)})\n', qrbug.Journals.DB)
-        feedback = f"Retiré l'enfant «{value}» à «{user_id}»\n"  # TODO: Error handling
+        if value == user_id:
+            feedback = f"<b>ERREUR :</b> L'ID du parent est identique à celui de l'enfant"
+        elif value not in qrbug.User[user_id].children_ids:
+            feedback = f"<b>ERREUR :</b> «{value}» n'est pas un enfant de «{user_id}»"
+        else:
+            qrbug.append_line_to_journal(f'user_remove({repr(user_id)}, {repr(value)})\n', qrbug.Journals.DB)
+            feedback = f"Retiré l'enfant «{value}» à «{user_id}»\n"
     else:
         feedback = "Unexpected edit failure for User\n"
     await request.write('<!DOCTYPE html>\n' + feedback)
