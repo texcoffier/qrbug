07ae6d58895fb139134c25c503eeaeee68a25378
Thierry EXCOFFIER <thierry.excoffier@univ-lyon1.fr>
2025-07-16 08:33:59 +0200

    Fix favicon if QRBug not a top level URL

--- ACTIONS/report_feedback.py
+++ ACTIONS/report_feedback.py
@@ -10,15 +10,23 @@ async def run(incidents, request):
     else:
         comment = ''
     await request.write(
-        f"""
-        <style>
+        f"""<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+    <link REL="icon" HREF="favicon.ico">
+    <style>
+        BODY {{ font-family: sans-serif }}
         DIV {{ margin: 2em; font-weight: bold; white-space: pre }}
         @media (orientation: portrait) {{ BODY {{ font-size: 5vw }} }}
-        </style>
-        <title>Rapport {html.escape(incident.thing_id)} {html.escape(incident.failure_id)}</title>
-        Merci d'avoir signalé l'incident :
-        <div>{html.escape(incident.failure.name('¤')).replace('¤', '<br>')}</div>
-        à propos de :
-        <div>{html.escape(incident.thing.name('¤')).replace('¤', '<br>')}</div>
-        {comment}
-        <p>Quelqu'un s'en occupera prochainement.""")
+   </style>
+</head>
+<body>
+    <title>Rapport {html.escape(incident.thing_id)} {html.escape(incident.failure_id)}</title>
+    Merci d'avoir signalé l'incident :
+    <div>{html.escape(incident.failure.name('¤')).replace('¤', '<br>')}</div>
+    à propos de :
+    <div>{html.escape(incident.thing.name('¤')).replace('¤', '<br>')}</div>
+    {comment}
+    <p>Quelqu'un s'en occupera prochainement.""")
